597ba3a17cc8a950666f7742904bd530a017aa0b
Jacob Lewinski <jdlewinski@outlook.com>
2023-11-22 11:17:18 -0600

    Create maui from command line

---
+++ CardGameTracker.Maui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CardGameTracker.Maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ CardGameTracker.Maui/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CardGameTracker.Maui;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
